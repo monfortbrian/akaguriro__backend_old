9d3c6db4599111d7d70b4c6c84a83adc5496e7c8
MBrian16 <monfortbrian@outlook.com>
2021-06-04 20:47:40 +0200

    just changed db.js

--- config/db.js
+++ config/db.js
@@ -1,15 +1,17 @@
 import mongoose from "mongoose";
 import { error, info } from "../utils/log.js";
 const NAMESPACE = "SERVER";
-const URI =
-  "mongodb+srv://bluetech:March_2021@cluster0.qbgzx.mongodb.net/ecommerce?ecommerce=true&w=majority";
+
 export const connectDb = () => {
   mongoose
-    .connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    })
+    .connect(
+      "mongodb+srv://bluetech:March_2021@cluster0.qbgzx.mongodb.net/ecommerce?ecommerce=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+      }
+    )
     .then((con) =>
       info(
         NAMESPACE,
